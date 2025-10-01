b760bf692943314fadd8888ece0e19108be64c24
Jordan <jordanduzabonfim@gmail.com>
2025-10-01 12:05:22 -0300

    feat: scheduler that selects a random movie for the next day's classic game

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"cinedle-backend/internal/config"
 	"cinedle-backend/internal/database"
+	classicService "cinedle-backend/internal/modules/classicGame/services"
+	movieService "cinedle-backend/internal/modules/movies/services"
 	"cinedle-backend/internal/router"
 	"cinedle-backend/internal/scheduler"
 )
@@ -11,7 +13,12 @@ func main() {
 	config.LoadConfig()
 	database.GetDBPool()
 
-	c := scheduler.StartFilmeDoDiaScheduler()
+	// cria o service
+	classicService := classicService.NewClassicGameService()
+	movieService := movieService.NewMoviesService()
+
+	// injeta no scheduler
+	c := scheduler.StartFilmeDoDiaScheduler(classicService, movieService)
 	defer c.Stop()
 
 	router.Run()
--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -2,18 +2,65 @@ package scheduler
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
+
+	"cinedle-backend/internal/modules/classicGame/models"
+	classic_game_service "cinedle-backend/internal/modules/classicGame/services"
+	movie_service "cinedle-backend/internal/modules/movies/services"
 
 	"github.com/robfig/cron/v3"
 )
 
-// Inicia o cron do filme do dia
-func StartFilmeDoDiaScheduler() *cron.Cron {
-	c := cron.New(cron.WithSeconds())
-	// Sortear filme às 16h
-	c.AddFunc("0 0 16 * * *", func() {
+func StartFilmeDoDiaScheduler(svc_classic classic_game_service.ClassicGameService, svc_movie movie_service.MoviesService) *cron.Cron {
+	c := cron.New()
+	// Sortear filme todo dia meia noite
+	c.AddFunc("0 0 * * *", func() {
 		fmt.Println("Sorteando filme do dia em:", time.Now())
-		// lógica de salvar no banco
+
+		movie_count, err := svc_movie.GetMovieCount()
+
+		if err != nil {
+			fmt.Println("Erro ao buscar quantidade de filmes")
+			return
+		}
+
+		var randomId int
+
+		for {
+			randomId = rand.Intn(movie_count-1) + 1 // sorteia entre 1 e movie_count
+			searchedGame, err := svc_classic.GetClassicGameById(randomId)
+			fmt.Println("Oq veio:", searchedGame)
+
+			if searchedGame.ID == 0 {
+				fmt.Println("ID vago:", randomId)
+				break
+			}
+
+			if err != nil {
+				fmt.Println("Erro ao buscar classicGame:")
+				break
+			}
+			fmt.Println("ID já existe, sorteando outro:", randomId)
+		}
+
+		// Data do próximo dia
+		tomorrow := time.Now().AddDate(0, 0, 1)
+
+		game := models.ClassicGame{
+			ID:           randomId,
+			Title:        "Filme sorteado",
+			Date:         tomorrow,
+			TotalGuesses: 0,
+		}
+
+		created, err := svc_classic.CreateClassicGame(game)
+		if err != nil {
+			fmt.Println("Erro ao criar jogo:", err)
+			return
+		}
+
+		fmt.Println("Jogo criado:", created)
 	})
 	c.Start()
 	return c
