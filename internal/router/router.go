8f911512b7121218434e6a326578282599d50a27
LuizGustavoNascimeto <luizgnf1@gmail.com>
2025-10-24 20:16:23 -0300

    Revert "documentation made with GO-Swagger"
    This reverts commit 6b5731c49eaf457b458f2ce3f70efd7a369b94fd.

--- internal/router/router.go
+++ internal/router/router.go
@@ -7,7 +7,6 @@ import (
 	movie_handler "cinedle-backend/internal/modules/movies/handlers"
 	movie_router "cinedle-backend/internal/modules/movies/routes"
 	movie_service "cinedle-backend/internal/modules/movies/services"
-	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -18,42 +17,6 @@ func Run() {
 	r := gin.Default()
 	r.Use(cors.Default())
 
-	// Serve o arquivo OpenAPI gerado para facilitar uso com Swagger UI
-	// acessível em: GET /openapi.yaml
-	r.GET("/openapi.yaml", func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Content-Type", "application/x-yaml")
-		c.File("./openapi.yaml")
-	})
-
-	// Página simples com Swagger UI (via CDN) em /docs
-	r.GET("/docs", func(c *gin.Context) {
-		html := `<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8" />
-		<title>Cinedle API Docs</title>
-		<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4/swagger-ui.css" />
-		<style>body { margin:0; padding:0; }</style>
-	</head>
-	<body>
-		<div id="swagger-ui"></div>
-		<script src="https://unpkg.com/swagger-ui-dist@4/swagger-ui-bundle.js"></script>
-		<script>
-			window.onload = function() {
-				const ui = SwaggerUIBundle({
-					url: '/openapi.yaml',
-					dom_id: '#swagger-ui',
-					presets: [SwaggerUIBundle.presets.apis],
-					layout: 'BaseLayout'
-				})
-			}
-		</script>
-	</body>
-</html>`
-
-		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(html))
-	})
 	//setup routes
 	movieService := movie_service.NewMoviesService()
 	movieHandler := movie_handler.NewMoviesHandler(movieService)
